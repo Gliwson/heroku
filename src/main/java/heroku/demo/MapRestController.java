75421e50a5250b5addd26b63e89ac96fc43fe41e
Mateusz Gliwka <gliwson@gmail.com>
2020-04-04 03:53:51 +0200

    init

--- src/main/java/heroku/demo/MapRestController.java
+++ src/main/java/heroku/demo/MapRestController.java
@@ -1,6 +1,7 @@
 package heroku.demo;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,7 @@ public class MapRestController {
         return "DONE";
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/test")
     public PointDTO reloadData2() {
         log.info("getAllProducts");
